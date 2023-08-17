d2cc508bed91bd7e9f18552cebdbb3ddae48ebd0
Michael Edes <michael.edes@boxmodel.com>
2023-08-17 21:23:43 +0100

    chore: setting up heroku

--- server/index.js
+++ server/index.js
@@ -156,3 +156,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Metro Sandwich Shop!");
+});
