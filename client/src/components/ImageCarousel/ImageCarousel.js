38cecf67cf2c4827339b1e2b77d86953fd0265b3
Michael Edes <michael.edes@boxmodel.com>
2023-08-17 21:29:04 +0100

    chore: adding endpoints

--- client/src/components/ImageCarousel/ImageCarousel.js
+++ client/src/components/ImageCarousel/ImageCarousel.js
@@ -10,7 +10,7 @@ function ImageCarousel({ cart, setCart }) {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8800/items")
+      .get("https://metro-sandwich-shop-df14f9a980e0.herokuapp.com/items")
       .then((response) => setContent(response.data))
       .catch((error) => console.error("Axios error:", error));
   }, []);
